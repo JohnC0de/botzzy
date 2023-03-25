0b4a671fcec216efd132f095d36b86e6d5b1b3c2
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2023-03-25 13:40:01 -0300

    fix: package deps

--- app/client/styles/global.css.ts
+++ app/client/styles/global.css.ts
@@ -2,23 +2,22 @@ import { createGlobalTheme, globalStyle } from "@vanilla-extract/css";
 import { colors, darkTheme, lightTheme } from "./theme/createTheme.css";
 import * as tokens from "./tokens";
 
-export const root = createGlobalTheme("html", tokens);
-export const vars = { ...root, colors };
+export const vars = createGlobalTheme(":root", { ...tokens, colors });
 
 globalStyle("*", {
-  padding: 0,
-  margin: 0,
-  boxSizing: "border-box",
-  fontFamily: vars.fonts.default,
+	padding: 0,
+	margin: 0,
+	boxSizing: "border-box",
+	fontFamily: vars.fonts.default,
 });
 
 globalStyle("li", {
-  listStyle: "none",
+	listStyle: "none",
 });
 
 globalStyle("body", {
-  backgroundColor: vars.colors.bg[500],
-  color: vars.colors.text[100],
+	backgroundColor: vars.colors.bg[500],
+	color: vars.colors.text[100],
 });
 
 export const globalStyles = { vars, darkTheme, lightTheme };
