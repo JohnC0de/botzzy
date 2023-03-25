a980453bf3569aa1982a0544d45894d8bcfccb3c
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2023-03-25 13:41:56 -0300

    fix: use nanoid

--- app/models/protectModels/FlowModel/functions/onDrop.ts
+++ app/models/protectModels/FlowModel/functions/onDrop.ts
@@ -1,41 +1,41 @@
-import uuid from "react-uuid";
+import { nanoid } from "nanoid";
 import type { DragEvent, RefObject } from "react";
 import type { ReactFlowInstance, Node } from "reactflow";
 
 type onDropProps = {
-  event: DragEvent<HTMLDivElement>;
-  reactFlowWrapperRef?: RefObject<HTMLDivElement>;
-  reactFlowInstance?: ReactFlowInstance<any, any> | null;
-  setNodes: (
-    value: React.SetStateAction<Node<{ label: string }, string | undefined>[]>
-  ) => void;
+	event: DragEvent<HTMLDivElement>;
+	reactFlowWrapperRef?: RefObject<HTMLDivElement>;
+	reactFlowInstance?: ReactFlowInstance<any, any> | null;
+	setNodes: (
+		value: React.SetStateAction<Node<{ label: string }, string | undefined>[]>,
+	) => void;
 };
 
 export function onDrop({
-  event,
-  reactFlowInstance,
-  reactFlowWrapperRef,
-  setNodes,
+	event,
+	reactFlowInstance,
+	reactFlowWrapperRef,
+	setNodes,
 }: onDropProps) {
-  event.preventDefault();
-  if (!reactFlowWrapperRef?.current || !reactFlowInstance) return;
+	event.preventDefault();
+	if (!reactFlowWrapperRef?.current || !reactFlowInstance) return;
 
-  const reactFlowBounds = reactFlowWrapperRef.current.getBoundingClientRect();
-  const type = event.dataTransfer.getData("application/reactflow");
+	const reactFlowBounds = reactFlowWrapperRef.current.getBoundingClientRect();
+	const type = event.dataTransfer.getData("application/reactflow");
 
-  if (typeof type === "undefined" || !type) return;
+	if (typeof type === "undefined" || !type) return;
 
-  const position = reactFlowInstance.project({
-    x: event.clientX - reactFlowBounds.left,
-    y: event.clientY - reactFlowBounds.top,
-  });
+	const position = reactFlowInstance.project({
+		x: event.clientX - reactFlowBounds.left,
+		y: event.clientY - reactFlowBounds.top,
+	});
 
-  const newNode: Node = {
-    id: uuid(),
-    type,
-    position,
-    data: { label: `${type} node` },
-  };
+	const newNode: Node = {
+		id: nanoid(),
+		type,
+		position,
+		data: { label: `${type} node` },
+	};
 
-  setNodes((nds) => nds.concat(newNode));
+	setNodes((nds) => nds.concat(newNode));
 }
